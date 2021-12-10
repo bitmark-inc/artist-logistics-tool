a4bfee2494026cc03e9a297bf67b5621039538d8
Jim Yeh <jim@bitmark.com>
2021-12-10 15:19:01 +0800

    Add two blank pages: welcome page and thank you page

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,11 +1,23 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import Welcome from '../views/Welcome.vue'
+import ThankYou from '../views/ThankYou.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/welcome',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/thankyou',
+    name: 'ThankYou',
+    component: ThankYou
   },
   // {
   //   path: '/about',
---
+++ ui/src/views/ThankYou.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is a Thank you page</h1>
+  </div>
+</template>
